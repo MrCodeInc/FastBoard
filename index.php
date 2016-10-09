8c494cf8ed0a27a5f7cdc04eec265e2fe5bab6ba
MrCode <MrCodeInc@users.noreply.github.com>
2016-10-09 19:26:50 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,24 @@
 <html>
 <title>FastBoard</title>
 <body>
-<h1>FastBoard</h1>
+<center> <h1>FastBoard</h1>
 <p> </p>
-<a href="https://github.com/">Github</a>
+<?php 
+echo <center> date("D M d, Y G:i a"); 
+?>
 <p> </p>
-<a href="https://google.com/">Google</a>
+<center> <a href="https://github.com/">Github</a>
 <p> </p>
-<a href="https://youtube.com/">YouTube</a>
+<center> <a href="https://google.com/">Google</a>
 <p> </p>
-<a href="https://twitter.com/">Twitter</a>
+<center> <a href="https://youtube.com/">YouTube</a>
 <p> </p>
-<a href="https://facebook.com/">Facebook</a>
+<center> <a href="https://twitter.com/">Twitter</a>
+<p> </p>
+<center> <a href="https://facebook.com/">Facebook</a>
 <p> </p>
 Created by <a href="https://github.com/MrCodeInc/">MrCodeInc.</a>
 You can view the source code <a href="https://github.com/MrCodeInc/FastBoard/">here.</a>
 </body>
 </html>
 
-<?php 
-echo date("D M d, Y G:i a"); 
-?>
