3cf723a91d606cc660ae7a19a1a9d2fc7f7ad620
MrCode <MrCodeInc@users.noreply.github.com>
2016-10-09 19:03:22 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+<title>FastBoard</title>
+<body>
+<h1>FastBoard</h1>
+<p> </p>
+<a href="https://github.com/">Github</a>
+<p> </p>
+<a href="https://google.com/">Google</a>
+<p> </p>
+<a href="https://youtube.com/">YouTube</a>
+<p> </p>
+<a href="https://twitter.com/">Twitter</a>
+<p> </p>
+<a href="https://facebook.com/">Facebook</a>
+<p> </p>
+Created by <a href="https://github.com/MrCodeInc/">MrCodeInc.</a>
+You can view the source code <a href="https://github.com/MrCodeInc/FastBoard/">here.</a>
+</body>
+</html>
+
+<?php 
+echo date("D M d, Y G:i a"); 
+?>
