a9cbcdfbf469d5b7de7c1ceca22daac7fdb29cac
MrCode <MrCodeInc@users.noreply.github.com>
2016-10-09 22:08:21 -0400

    Update index.php

--- index.php
+++ index.php
@@ -9,11 +9,11 @@ echo date("Y");
 <?php
 
 $messages = array(
-    'The best of best PHP skillz.',
-    'Follow me on github.',
+    'The best of best PHP skillz.'
+    'Follow me on github.'
     'Chillin like a villain.'
-    'MrCodeInc for president 2016',
-    'I'M READY PROMOTION! I'M READY! PROMOTION! I'M READY! PROMOTION!',
+    'MrCodeInc for president 2016'
+    'I'M READY PROMOTION! I'M READY! PROMOTION! I'M READY! PROMOTION!'
     'Gotta go fast......board'
 );
 
